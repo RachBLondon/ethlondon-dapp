255e7bc1006b4d632d2d9839ea542d3d606f06f0
xiko <Kobuta23>
2020-03-01 00:56:47 +0000

    homepage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Form, Button, Navbar, Nav, Card, Container, Row, Col } from 'react-bootstrap';
-import { Name } from './Constants';
-import Home from './Home';
-import Game from './Game';
-import Box from '3box'
+import {
+  Form,
+  Button,
+  Navbar,
+  Nav,
+  Card,
+  Container,
+  Row,
+  Col
+} from "react-bootstrap";
+import { Name } from "./Constants";
+import Home from "./Home";
+import Game from "./Game";
+import Box from "3box";
 
 export default class App extends Component {
-
   state = {
-    needToAWeb3Browser: false,
-  }
+    needToAWeb3Browser: false
+  };
 
   async getAddressFromMetaMask() {
     if (typeof window.ethereum == "undefined") {
@@ -24,53 +32,58 @@ export default class App extends Component {
   async componentDidMount() {
     await this.getAddressFromMetaMask();
     if (this.state.accounts) {
-      const profile = await Box.getProfile(this.state.accounts[0])
-      console.log('profile', profile)
-      const spaceList = await Box.listSpaces(this.state.accounts[0])
-      console.log('spacelist', spaceList)
-      const followers = await Box.getSpace(this.state.accounts[0], 'MyFollowers');
-      console.log("followers", followers)
+      const profile = await Box.getProfile(this.state.accounts[0]);
+      console.log("profile", profile);
+      const spaceList = await Box.listSpaces(this.state.accounts[0]);
+      console.log("spacelist", spaceList);
+      const followers = await Box.getSpace(
+        this.state.accounts[0],
+        "MyFollowers"
+      );
+      console.log("followers", followers);
       // Now MetaMask's provider has been enabled, we can start working with 3Box
     }
   }
   render() {
     if (this.state.needToAWeb3Browser) {
-      return <h1>Please install metamask</h1>
+      return <h1>Please install metamask</h1>;
     }
-
 
     return (
       <Router>
         <div>
-          <Navbar bg="light" expand="lg" style={{ minHeight: '40px' }}>
+          <Navbar bg="light" expand="lg" style={{ minHeight: "40px" }}>
             {this.state.accounts && (
-              <Nav fill style={{ width: "100%" }} >
-                <Nav.Item><Link to="/">Home</Link></Nav.Item>
-                <Nav.Item><Link to="/profile">Profile Update</Link></Nav.Item>
-                <Nav.Item><Link to="/game">Game</Link></Nav.Item>
+              <Nav fill style={{ width: "100%" }}>
+                <Nav.Item>
+                  <Link to="/">Home</Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Link to="/profile">Profile Update</Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Link to="/game">Game</Link>
+                </Nav.Item>
               </Nav>
             )}
-
           </Navbar>
-          <div className="container" style={{ paddingTop: '50px' }}>
-            <h1>{Name}</h1>
-            <p>Save together</p>
-            {this.state.needToAWeb3Browser && <h2>Please install metamask🦊</h2>}
-            {(!this.state.needToAWeb3Browser && !this.state.accounts) && <h2>Connect MetaMask🤝</h2>}
+          <div className="container" style={{ paddingTop: "50px" }}>
+            {this.state.needToAWeb3Browser && (
+              <h2>Please install metamask🦊</h2>
+            )}
+            {!this.state.needToAWeb3Browser && !this.state.accounts && (
+              <h2>Connect MetaMask🤝</h2>
+            )}
             {this.state.accounts && (
               <Switch>
                 <Route path="/profile">
-                  <Profile
-                    ethAddress={this.state.accounts[0]}
-                  />
+                  <Profile ethAddress={this.state.accounts[0]} />
                 </Route>
                 <Route path="/game">
                   <Game />
                 </Route>
                 <Route path="/">
-                  <Home
-                    ethAddress={this.state.accounts[0]}
-                  />
+                  <Home ethAddress={this.state.accounts[0]} />
                 </Route>
               </Switch>
             )}
@@ -81,17 +94,12 @@ export default class App extends Component {
   }
 }
 
-
-
-
 class Profile extends Component {
   render() {
-    return (<>
-      <h1>Profile</h1>
-    </>);
+    return (
+      <>
+        <h1>Profile</h1>
+      </>
+    );
   }
 }
-
-
-
-
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,67 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import Carousel from "react-bootstrap/Carousel";
 
 export default class Home extends Component {
-    render() {
-      return (<>
-        <h1>Home</h1>
-        <h2>{this.props.ethAddress}</h2>
-      </>);
-    }
+  render() {
+    return (
+      <div className="home">
+        <div className="fullpager">
+          <div className="banner">
+            <h1>Join the QuaranTeam</h1>
+            <h3>Check in every week with</h3>
+            <h1>9.90DAI</h1>
+          </div>
+        </div>
+        <div className="fullpager">
+          <div className="banner">
+            <h4>Check in every two weeks for the whole year</h4>
+            <br />
+            <br />
+            <h4>The interest pot is split amongst loyal savers</h4>
+            <br />
+            <br />
+            <h4>Everyone gets back 100% of their principal</h4>
+          </div>
+        </div>
+        <div className="fullpager">
+          <Carousel wrap={false} slide={false}>
+            <Carousel.Item>
+              <img className="d-block w-100" src={`./assets/1.svg`} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={`./assets/2.svg`} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={`./assets/3.svg`} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={`./assets/4.svg`} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={`./assets/5.svg`} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={`./assets/6.svg`} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={`./assets/7.svg`} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={`./assets/8.svg`} />
+            </Carousel.Item>
+          </Carousel>
+        </div>
+      </div>
+    );
   }
+}
+
+const items = [];
+
+for (var i = 1; i < 8; i++) {
+  items.push(
+    <Carousel.Item>
+      <img className="d-block w-100" src={`./public/${i}.svg`} />
+    </Carousel.Item>
+  );
+}
